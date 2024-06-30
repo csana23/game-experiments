08a42267e966febcae868a76b64e182a1ffaec0f
Richard Csanaki <richard.csanaki@gmail.com>
2024-06-30 21:05:59 +0200

    game logic seems to be working

--- tic-tac-toe/experiments.py
+++ tic-tac-toe/experiments.py
@@ -6,28 +6,60 @@ print(game_space)
 
 pattern = r'^[0-2][0-2]$'
 
-def check_win(game_space: np.array):
-    
+def check_win(game_space: np.array, computer_character: np.float64, player_character: np.float64):
+    is_won = False
+
+    # rows
+    for row in range(0,3):
+        if game_space[row][0] == game_space[row][1] == game_space[row][2] == computer_character:
+            is_won = True
+            print("Computer is_won!")
+
+        elif game_space[row][0] == game_space[row][1] == game_space[row][2] == player_character:
+            is_won = True
+            print("Player is_won!")
+
+    # columns
+    for col in range(0,3):
+        if game_space[0][col] == game_space[1][col] == game_space[2][col] == computer_character:
+            is_won = True
+            print("Computer is_won!")
+        
+        elif game_space[0][col] == game_space[1][col] == game_space[2][col] == player_character:
+            is_won = True
+            print("Player is_won!")
+
+    # diagonal
+    if (game_space[0][0] == game_space[1][1] == game_space[2][2] == computer_character) or (game_space[0][2] == game_space[1][1] == game_space[2][0] == computer_character):
+        is_won = True
+        print("Computer is_won!")
+
+    elif (game_space[0][0] == game_space[1][1] == game_space[2][2] == player_character) or (game_space[0][2] == game_space[1][1] == game_space[2][0] == player_character):
+        is_won = True
+        print("Player is_won!")
+
+    return is_won
 
 while True:
-    user_input = input("Enter coordinates:\n")
+    player_input = input("Enter coordinates:\n")
 
-    if re.match(pattern=pattern, string=user_input):
-        print(user_input, "OK")
+    if re.match(pattern=pattern, string=player_input):
+        print(player_input, "OK")
         
-        user_input_row = int(user_input[0])
-        user_input_col = int(user_input[1])
+        player_input_row = int(player_input[0])
+        player_input_col = int(player_input[1])
 
-        if np.isnan(game_space[user_input_row][user_input_col]):
-            game_space[user_input_row][user_input_col] = 0
+        if np.isnan(game_space[player_input_row][player_input_col]):
+            game_space[player_input_row][player_input_col] = 0
             print(game_space)
 
-            # check if game is won
-            if 
+            # check if game is is_won
+            if check_win(game_space, np.float64(1), np.float64(0)) == True:
+                break
         else:
             print("Field already filled!")
 
 
 
     else:
-        print(user_input, "NOT OK")
+        print(player_input, "NOT OK")
--- tic-tac-toe/no_graphics.py
+++ tic-tac-toe/no_graphics.py
@@ -1,16 +1,49 @@
 import numpy as np
 import random
+import re
+
+pattern = r'^[0-2][0-2]$'
+
+def check_win(game_space: np.array, computer_character: np.float64, player_character: np.float64):
+    is_won = False
+
+    # rows
+    for row in range(0,3):
+        if game_space[row][0] == game_space[row][1] == game_space[row][2] == computer_character:
+            is_won = True
+            print("Computer is_won!")
+
+        elif game_space[row][0] == game_space[row][1] == game_space[row][2] == player_character:
+            is_won = True
+            print("Player is_won!")
+
+    # columns
+    for col in range(0,3):
+        if game_space[0][col] == game_space[1][col] == game_space[2][col] == computer_character:
+            is_won = True
+            print("Computer is_won!")
+        
+        elif game_space[0][col] == game_space[1][col] == game_space[2][col] == player_character:
+            is_won = True
+            print("Player is_won!")
+
+    # diagonal
+    if (game_space[0][0] == game_space[1][1] == game_space[2][2] == computer_character) or (game_space[0][2] == game_space[1][1] == game_space[2][0] == computer_character):
+        is_won = True
+        print("Computer is_won!")
+
+    elif (game_space[0][0] == game_space[1][1] == game_space[2][2] == player_character) or (game_space[0][2] == game_space[1][1] == game_space[2][0] == player_character):
+        is_won = True
+        print("Player is_won!")
+
+    return is_won
 
 # create game space
 game_space = np.array([[np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan]])
 print("Initial state:\n", game_space)
 
-# choose computer and player characters: 0 for o, 1 for x
-computer_character = random.randint(0,1)
-print("Computer character:", computer_character, "o" if computer_character == 0 else "x")
-
-player_character = 0 if computer_character == 1 else 1
-print("Player character:", player_character, "o" if player_character == 0 else "x")
+computer_character = 1
+player_character = 0
 
 # computer takes first step
 first_computer_step = (random.randint(0,2), random.randint(0,2))
@@ -24,14 +57,30 @@ i = 0
 # game loop
 while np.isnan(game_space).any():
     try:
+        print("Iteration:", i)
+
         # get player step TODO
         while True:
             player_input = input("Enter choice (eg. 12):\n")
 
-            if 
+            if re.match(pattern=pattern, string=player_input):
+                print(player_input, "OK")
 
+                player_input_row = int(player_input[0])
+                player_input_col = int(player_input[1])
 
-        print("Iteration:", i)
+                if np.isnan(game_space[player_input_row][player_input_col]):
+                    game_space[player_input_row][player_input_col] = player_character
+                    print(game_space)
+
+                    break
+                else:
+                    print("Field already filled!")
+
+        # check if game is is_won after player step
+        if check_win(game_space, np.float64(1), np.float64(0)) == True:
+            break   
+
         nan_elements_idx = np.argwhere(np.isnan(game_space)) 
         print(nan_elements_idx)
 
@@ -40,6 +89,10 @@ while np.isnan(game_space).any():
         
         game_space[tuple(nan_elements_idx[computer_step])] = computer_character
         print(game_space)
+
+        # check if game is is_won after computer step
+        if check_win(game_space, np.float64(1), np.float64(0)) == True:
+            break
 
         i = i+1
     except Exception as e:
